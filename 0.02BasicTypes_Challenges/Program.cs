7d5650349750e3df59e1c8f17cd814f1a6d88dc5
Shelby Grice <Shelby Grice>
2017-03-22 12:13:21 -0400

    Learned about basic types and strings

---
+++ 0.02BasicTypes_Challenges/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02BasicTypes_Challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+            //int numberOfDays;
+
+            string name = "Shelby";
+            //string build;
+
+            //bool isAlive = true;
+            //bool isDead;
+
+            //float f;
+            //float temperature = 6.23f;
+
+            //double d = 7.93;
+            //double g;
+
+            //decimal money;
+            //decimal inchesOfRain = 54.234124m;
+
+
+            //Console.WriteLine(n);
+            //Console.WriteLine(name);
+            //Console.WriteLine(t);
+            //Console.WriteLine(l);
+            //Console.WriteLine(d);
+            //Console.WriteLine(s);
+            //Console.ReadLine();
+
+
+         
+
+            Console.WriteLine("{0} is {1} years old.", name, age);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 0.03_Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._03_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Simple Demo
+            string firstName;
+            string lastName;
+            string occupation;
+
+            firstName = "Shelby";
+            lastName = "Grice";
+            occupation = "Student";
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            //String Interpolation
+            Console.WriteLine("Name: {0}" + "\n" + "Occupation: {1}", fullName, occupation);
+            Console.ReadLine();
+        }
+    }
+}
