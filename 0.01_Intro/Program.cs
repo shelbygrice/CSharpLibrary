12268c9e9d3810f2bfd368ffcc7436c2b57229d9
Shelby Grice <Shelby Grice>
2017-03-22 10:13:21 -0400

    Console Intro

---
+++ 0.01_Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace _0._01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
